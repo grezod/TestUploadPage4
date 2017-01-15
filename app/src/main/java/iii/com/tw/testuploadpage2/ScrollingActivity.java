3889154a236853e03826262b30d84ba69e82630b
grezod <grezod@gmail.com>
2017-01-15 22:43:36 +0800

    switch btn done

--- app/src/main/java/iii/com/tw/testuploadpage2/ScrollingActivity.java
+++ app/src/main/java/iii/com/tw/testuploadpage2/ScrollingActivity.java
@@ -8,13 +8,42 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private View.OnClickListener imgBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.imgBtn1:
+                    Toast.makeText(ScrollingActivity.this,"String.valueOf(R.id.imgBtn1)",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.imgBtn2:
+                    Toast.makeText(ScrollingActivity.this,"String.valueOf(R.id.imgBtn2)",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.imgBtn3:
+                    Toast.makeText(ScrollingActivity.this,"String.valueOf(R.id.imgBtn3)",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.imgBtn4:
+                    Toast.makeText(ScrollingActivity.this,"String.valueOf(R.id.imgBtn4)",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.imgBtn5:
+                    Toast.makeText(ScrollingActivity.this,"String.valueOf(R.id.imgBtn5)",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            Toast.makeText(ScrollingActivity.this,"END",Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        init();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +55,24 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void init() {
+        //**************
+        imgBtn1 = (ImageButton)findViewById(R.id.imgBtn1);
+        imgBtn1.setOnClickListener(imgBtnClick);
+        //**************
+        imgBtn2 = (ImageButton)findViewById(R.id.imgBtn2);
+        imgBtn2.setOnClickListener(imgBtnClick);
+        //**************
+        imgBtn3 = (ImageButton)findViewById(R.id.imgBtn3);
+        imgBtn3.setOnClickListener(imgBtnClick);
+        //**************
+        imgBtn4 = (ImageButton)findViewById(R.id.imgBtn4);
+        imgBtn4.setOnClickListener(imgBtnClick);
+        //**************
+        imgBtn5 = (ImageButton)findViewById(R.id.imgBtn5);
+        imgBtn5.setOnClickListener(imgBtnClick);
     }
 
     @Override
@@ -48,4 +95,12 @@ public class ScrollingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    //*********************
+    ImageButton imgBtn1;
+    ImageButton imgBtn2;
+    ImageButton imgBtn3;
+    ImageButton imgBtn4;
+    ImageButton imgBtn5;
+    //*********************
 }
