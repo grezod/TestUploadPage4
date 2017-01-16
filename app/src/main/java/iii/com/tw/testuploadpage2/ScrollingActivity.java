b04a3fb181cc5ea28263fd639313d01f52be0df2
grezod <grezod@gmail.com>
2017-01-16 21:46:02 +0800

    first imgBtn complete

--- app/src/main/java/iii/com/tw/testuploadpage2/ScrollingActivity.java
+++ app/src/main/java/iii/com/tw/testuploadpage2/ScrollingActivity.java
@@ -1,21 +1,50 @@
 package iii.com.tw.testuploadpage2;
 
-import android.os.Bundle;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import static android.Manifest.permission.*;
+import static android.R.attr.bitmap;
+
+
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 
 public class ScrollingActivity extends AppCompatActivity {
+    private static final int REQUEST_READ_STORAGE = 3;
 
     private View.OnClickListener imgBtnClick = new View.OnClickListener() {
+
         @Override
         public void onClick(View v) {
             switch (v.getId()){
@@ -36,13 +65,95 @@ public class ScrollingActivity extends AppCompatActivity {
                     break;
             }
             Toast.makeText(ScrollingActivity.this,"END",Toast.LENGTH_SHORT).show();
+
+            //**
+            Intent intent = new Intent();
+            //開啟Pictures畫面Type設定為image
+            intent.setType("image/*");
+            //使用Intent.ACTION_GET_CONTENT這個Action                                            //會開啟選取圖檔視窗讓您選取手機內圖檔
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            //取得相片後返回本畫面
+            startActivityForResult(intent, 1);
+            //**
         }
     };
+
+    //****
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //**
+        if (resultCode == RESULT_OK) {
+            //取得圖檔的路徑位置
+            Uri uri = data.getData();
+            //寫log
+           // Log.e("uri", uri.toString());
+            //抽象資料的接口
+            Toast.makeText(ScrollingActivity.this,"11",Toast.LENGTH_SHORT).show();
+
+
+            ContentResolver cr = this.getContentResolver();
+            try {
+                //由抽象資料接口轉換圖檔路徑為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                //取得圖片控制項ImageView
+                ImageButton imageView = (ImageButton) findViewById(R.id.imgBtn1);
+                // 將Bitmap設定到ImageView
+                imageView.setImageBitmap(bitmap);
+
+
+                /**
+
+                //將 Bitmap 轉為 base64 字串
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                byte[] bitmapData = bos.toByteArray();
+                String imageBase64 = Base64.encodeToString(bitmapData, Base64.DEFAULT);
+                //Log.d("editor",imageBase64);
+
+                //將圖檔上傳至 Imgur，將取得的圖檔網址插入文字輸入框
+
+                imgurUpload(imageBase64); //程式寫在後面
+
+                */
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(),e);
+            }
+
+
+
+
+        }
+        //**
+    }
+
+    //*************
+
+    //**
+
+
+    //**
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+
+        int permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            //未取得權限，向使用者要求允許權限
+            ActivityCompat.requestPermissions( this,
+                    new String[]{READ_EXTERNAL_STORAGE},
+                    REQUEST_READ_STORAGE );
+        }else{
+            //已有權限，可進行檔案存取
+
+        }
+
         init();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
