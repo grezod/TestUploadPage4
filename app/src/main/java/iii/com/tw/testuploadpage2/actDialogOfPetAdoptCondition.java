b3a28f4a267969e12e30764c324a84cac0042569
grezod <grezod@gmail.com>
2017-02-03 01:49:31 +0800

    將彈跳視窗設為無法點空白處結束 按下確認後才結束

--- app/src/main/java/iii/com/tw/testuploadpage2/actDialogOfPetAdoptCondition.java
+++ app/src/main/java/iii/com/tw/testuploadpage2/actDialogOfPetAdoptCondition.java
@@ -3,6 +3,8 @@ package iii.com.tw.testuploadpage2;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class actDialogOfPetAdoptCondition extends Activity {
 
@@ -10,5 +12,12 @@ public class actDialogOfPetAdoptCondition extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_dialog_of_pet_adopt_condition);
+        Button btnSubmit =(Button)findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
